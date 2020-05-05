a37bda44ff55e2afe2e9530ba3f27dbd16a798fe
marc514 <zuohao183@163.com>
2020-05-05 15:04:42 +0800

    git commit -m 'grammar'

--- modules/routing/routing_component.h
+++ modules/routing/routing_component.h
@@ -34,6 +34,7 @@ class RoutingComponent final
   bool Proc(const std::shared_ptr<RoutingRequest>& request) override;
 
  private:
+  // ‘::’位于开头 means 全局命名空间
   std::shared_ptr<::apollo::cyber::Writer<RoutingResponse>> response_writer_ =
       nullptr;
   std::shared_ptr<::apollo::cyber::Writer<RoutingResponse>>
